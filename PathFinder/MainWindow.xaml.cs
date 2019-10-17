865aa376dd07ba13ee4621d605d8c3464c85a0e2
jacek <jacekbulowski.kontakt@gmail.com>
2019-10-17 03:26:29 +0200

    Improved comments.

--- PathFinder/MainWindow.xaml.cs
+++ PathFinder/MainWindow.xaml.cs
@@ -100,18 +100,26 @@ namespace PathFinderWPF
             Cell startCell = grid.cells[grid.startCell.row, grid.startCell.column];
             Cell endCell = grid.cells[grid.endCell.row, grid.endCell.column];
 
+            // contains cells that may lead to end cell
             var openSet = new List<Cell>();
+
+            // contains cells 
             var closedSet = new HashSet<Cell>();
 
+            // contains cells whose costs were calculated
+            // and either will be used for path or not
+            // depending of the values of the costs
             openSet.Add(startCell);
 
             while (openSet.Count > 0)
             {
-
                 Cell currentCell = openSet[0];
                 
                 for (var i = 1; i < openSet.Count; i++)
                 {
+                    // currentCell needs to be the current closest known cell
+                    // openSet contains neighbours that may have a bigger fCost
+                    // we need to iterate and find the cell that is the closest to the end cell
                     if (openSet[i].fCost < currentCell.fCost || openSet[i].fCost == currentCell.fCost && openSet[i].hCost < currentCell.hCost)
                         currentCell = openSet[i];
                 }
@@ -122,19 +130,20 @@ namespace PathFinderWPF
                 // path found
                 if (currentCell == endCell || currentCell.type == ButtonType.End)
                 {
-                    AnimateUI(startCell, endCell);
+                    BuildPathAndAnimateUI(startCell, endCell);
                     return;
                 }
 
+                // if path not found yet find neighbours of current cell
+                // than calculate their costs(g, h, f) and set its parent
+                // to currentCell
                 foreach (var neighbour in grid.NeighboursOf(currentCell))
                 {
                     if (!neighbour.IsCellWalkable || closedSet.Contains(neighbour))
                         continue;
 
-                    // int newCostToNeighbour = node.gCost + GetDistance(node, neighbour);
                     int newCostToNeighbour = currentCell.gCost + grid.getDistanceBetweenCells(currentCell, neighbour);
 
-                    // if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
                     if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
                     {
                         // we need to calculate the new fCost
@@ -147,7 +156,6 @@ namespace PathFinderWPF
                             openSet.Add(neighbour);
                     }
                 }
-
             }
         }
 
@@ -188,36 +196,35 @@ namespace PathFinderWPF
             ResetButton.Content = "Reset";
         }
 
-        private void AnimateUI(Cell startCell, Cell endCell)
+        private void BuildPathAndAnimateUI(Cell startCell, Cell endCell)
         {
-            grid.RetracePath(startCell, endCell);
+            // Create a path list so we know what cells
+            // to animate
+            var path = grid.RetracePath(startCell, endCell);
 
             foreach (Cell cell in grid.cells)
             {
-                if (grid.path != null)
+                if (path != null)
                 {
-                    if (grid.path.Contains(cell))
+                    if (path.Contains(cell))
                     {
-
                         // Removing end cell from path
                         // so it doesnt get animated
-                        grid.path.Remove(endCell);
+                        path.Remove(endCell);
 
                         foreach (Button button in buttons)
                         {
-
                             // Using full namespace, because it collides
                             // with the PathFinderWPF.Grid class
                             var row = System.Windows.Controls.Grid.GetRow(button);
                             var column = System.Windows.Controls.Grid.GetColumn(button);
 
-                            foreach (Cell pathCell in grid.path)
+                            foreach (Cell pathCell in path)
                             {
                                 if (pathCell.row == row && pathCell.column == column)
                                 {
                                     button.Background = Brushes.Red;
                                     button.Content = findPathAttempt;
-
                                 }
                             }
                         }
